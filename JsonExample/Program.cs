66209e9f2c8526d715c6c5eff81cc89b1405356e
Kristupas <kristupas.tadas@gmail.com>
2025-04-29 23:53:37 +0300

    u1 galutinis

--- JsonExample/Program.cs
+++ JsonExample/Program.cs
@@ -1,10 +1,26 @@
-﻿namespace JsonExample
+﻿using Newtonsoft.Json;
+
+namespace JsonExample
 {
     internal class Program
     {
+        public class Darbuotojas
+        {
+            public string Vardas { get; set; }
+            public int Amzius { get; set; }
+            public string Miestas { get; set; }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string failoKelias = "C:\\Users\\krist\\OneDrive\\Desktop\\C#NuGET\\JsonExample\\JsonExample\\DarbuotojaiJSON.json";
+
+            string jsonAtsakymas = File.ReadAllText(failoKelias);
+
+            Darbuotojas darbuotojas = JsonConvert.DeserializeObject<Darbuotojas>(jsonAtsakymas);
+
+            Console.WriteLine($"Name: {darbuotojas.Vardas}");
+            Console.WriteLine($"Age: {darbuotojas.Amzius}");
+            Console.WriteLine($"City: {darbuotojas.Miestas}");
         }
     }
 }
