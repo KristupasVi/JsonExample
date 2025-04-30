c450026cca42a0beba9e4a8b085972ff6079dafc
Kristupas <kristupas.tadas@gmail.com>
2025-04-30 16:13:19 +0300

    Sukurtas Admin ir Vartotojas objektai

--- JsonExample/Program.cs
+++ JsonExample/Program.cs
@@ -10,18 +10,30 @@ namespace JsonExample
             public int Amzius { get; set; }
             public string Miestas { get; set; }
         }
+
+        public class Adminas : Darbuotojas
+        {
+            public string Role { get; set; } = "adminas";
+            public bool priziuretojas { get; set; } = true;
+        }
+
+        public class Programuotojas : Darbuotojas {
+            public string Role { get; set; } = "vartotojas";
+            public bool priziuretojas { get; set; } = false;
+
+        }
         static void Main(string[] args)
         {
             string failoKelias = "C:\\Users\\krist\\OneDrive\\Desktop\\C#NuGET\\JsonExample\\JsonExample\\DarbuotojaiJSON.json";
 
             string jsonAtsakymas = File.ReadAllText(failoKelias);
 
-            Darbuotojas darbuotojas = JsonConvert.DeserializeObject<Darbuotojas>(jsonAtsakymas);
+            List<Darbuotojas> darbuotojai = JsonConvert.DeserializeObject<List<Darbuotojas>>(jsonAtsakymas);
 
-           
-            Console.WriteLine($"Vardas: {darbuotojas.Vardas}");
-            Console.WriteLine($"Amzius: {darbuotojas.Amzius}");
-            Console.WriteLine($"Miestas: {darbuotojas.Miestas}");
+            foreach (var darbuotojas in darbuotojai)
+            {
+                Console.WriteLine($"Vardas: {darbuotojas.Vardas}, Amzius:{darbuotojas.Amzius}, Miestas: {darbuotojas.Miestas}");
+            }
         }
     }
 }
